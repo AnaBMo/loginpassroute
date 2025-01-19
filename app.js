174750a13c502c8caa07cc768c78874b371f9ddf
AnaBMo <anab_mj@outlook.com>
2025-01-19 22:56:51 +0100

    completo

--- app.js
+++ app.js
@@ -8,9 +8,17 @@ dotenv.config();
 const app = express();
 const PORT = 4000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 middlewares.setupAPP(app);
 routes.setup(app);
 
+app.use(session({
+  secret: process.env.PALABRA_SECRETA || 'secretoSuperSecreto',
+  resave: false,
+  saveUninitialized: true,
+}));
+
 app.listen(PORT, () => {
 console.log(`Servidor en ejecución en http://localhost:${PORT}`);
-});
+});app.use(bodyParser.urlencoded({ extended: true }));
